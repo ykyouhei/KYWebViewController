279bea3dae9fae0975a5a90a581d28d5eac6ff17
kyyamagu <kyyamagu@r.recruit.co.jp>
2015-12-30 10:51:04 +0900

    Initial Commit

---
+++ KYWebViewController/KYWebViewController.h
@@ -0,0 +1,19 @@
+//
+//  KYWebViewController.h
+//  KYWebViewController
+//
+//  Created by 山口　恭兵 on 2015/12/30.
+//  Copyright © 2015年 kyo__hei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KYWebViewController.
+FOUNDATION_EXPORT double KYWebViewControllerVersionNumber;
+
+//! Project version string for KYWebViewController.
+FOUNDATION_EXPORT const unsigned char KYWebViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KYWebViewController/PublicHeader.h>
+
+
