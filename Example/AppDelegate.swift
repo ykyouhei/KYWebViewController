0fe8fbf00f65f3e4ba6720a1be979c755b827a7e
ykyouhei <kyouhei.lab@gmail.com>
2015-12-30 16:57:48 +0900

    add Resorces.

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import KYWebViewController
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        window?.rootViewController = UINavigationController(rootViewController: KYWebViewController(URL: NSURL(string: "https://github.com/")!))
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
